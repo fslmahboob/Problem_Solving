49f2eb6257af0db693c1963c42da2501a3a0e0aa
faisal <faisal.mahboob@hotmail.com>
2023-08-05 19:59:17 +1000

    Initial commit for Think Python

---
+++ ThinkPython2/ThinkPython2_Ch16_ExA.py
@@ -0,0 +1,24 @@
+"""
+As an exercise, write a function called print_time that takes a Time object and prints it in
+the form hour:minute:second. Hint: the format sequence '%.2d' prints an integer using
+at least two digits, including a leading zero if necessary.
+(Think Python 2, page 155)
+
+Skills:
+ -F-Strings
+ -Working with Time objects
+
+Status: Complete
+"""
+class Time:
+    """Represents the time of day.
+    attributes: hour, minute, second
+    """
+
+
+time = Time()
+time.hour = 11
+time.min = 59
+time.sec = 30
+
+print(f"{time.hour:02d}:{time.min:02d}:{time.sec:02d}") # good cheat sheet with examples: https://www.pythonmorsels.com/string-formatting/#cheat-sheets
