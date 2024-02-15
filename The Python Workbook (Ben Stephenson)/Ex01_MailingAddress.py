928bdada65441a4cb1b8f8846ee133ec9980fa4e
faisal <faisal.mahboob@hotmail.com>
2024-02-15 21:29:07 +1100

    Practicing python

---
+++ The Python Workbook (Ben Stephenson)/Ex01_MailingAddress.py
@@ -0,0 +1,14 @@
+"""
+Exercise 01: Mailing Address
+(Solved, 9 Lines)
+Create a program that displays your name and complete mailing address. The address
+should be printed in the format that is normally used in the area where you live. Your
+program does not need to read any input from the user.
+
+Status: Complete
+
+Skills:
+ -printing
+"""
+
+print("128 Eskaton Road, Dhaka, 1000")
---
+++ The Python Workbook (Ben Stephenson)/Ex02_Hello.py
@@ -0,0 +1,16 @@
+"""
+Exercise 2: Hello
+(Solved, 9 Lines)
+Write a program that asks the user to enter his or her name. The program should
+respond with a message that says hello to the user, using his or her name.
+
+Status: Complete
+
+Skills:
+ -Input from user
+ -String formatting
+"""
+
+a = input("What is your name?")
+
+print(f"Hello, {a}.")
